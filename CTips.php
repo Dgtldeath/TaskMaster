887c2b34b103513684dafa5c03859f120c8be938
Adam Gumm <adam.gumm@gmail.com>
2024-07-17 00:31:18 -0400

    Update CTips.php
    minor comment.

--- CTips.php
+++ CTips.php
@@ -20,6 +20,8 @@ class CTips
         $this->tips[] = "You can mark a task as 'Already Complete' when you create it so you don't have to check it off later.";
         $this->tips[] = "If you type the Category Name in your Task Title, TaskMaster ai will automatically set the Category for you.";
         $this->tips[] = "If your new task title has the same name as an old task, the new task Category will automatically be set based on the old task.";
+
+        // add more tips
     }
 
     public function getTip() {
